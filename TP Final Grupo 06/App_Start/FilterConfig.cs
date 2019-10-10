02bb02f8a206912ad313dbe241ccafee571afed1
LUCA FABRIZIO VILA <44713304@ort>
2019-10-10 09:55:29 -0300

    Agregar archivos de proyecto.

---
+++ TP Final Grupo 06/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TP_Final_Grupo_06
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TP Final Grupo 06/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP_Final_Grupo_06.MvcApplication" Language="C#" %>
