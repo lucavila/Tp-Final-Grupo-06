d74e09b1a30d61f8b222f69e4ad4dda20c15b436
Ivan Szmelc <44851637@comunidadort.edu.ar>
2019-11-07 09:51:27 -0300

    sda

---
+++ TP Final Grupo 06/App_Start/IdentityConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP_Final_Grupo_06.App_Start
+{
+    public class IdentityConfig
+    {
+    }
+}
