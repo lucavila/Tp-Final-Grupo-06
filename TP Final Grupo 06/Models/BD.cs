c39474061b52f45c6e59bd5d8d304817af5392a3
Ivan Szmelc <44851637@comunidadort.edu.ar>
2019-11-14 08:18:19 -0300

    dsad

--- TP Final Grupo 06/Models/BD.cs
+++ TP Final Grupo 06/Models/BD.cs
@@ -76,15 +76,5 @@ namespace TP_Final_Grupo_06.Models
             return lista_local;
         }
 
-        public static Usuario Login(Usuario miUsuario)
-        {
-            SqlConnection conn = Conectar();
-            Usuario unUsuario = null;
-            SqlCommand consulta = conn.CreateCommand();
-            consulta.CommandText = "sp_LogIn";
-            consulta.CommandType = System.Data.CommandType.StoredProcedure;
-            consulta.Parameters.AddWithValue("@usu",miUsuario.NombreDeUsuario)
-            consulta.Parameters.AddWithValue("@pass",miUsuario.Contraseña)
-        }
     }
 }
